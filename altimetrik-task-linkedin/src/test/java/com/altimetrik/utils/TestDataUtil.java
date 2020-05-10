1b77bcc453487782c1fa5d47064c4d8dcca20dcf
tejaswirajamane <rajamanetejaswi518@gmail.com>
2020-05-10 18:52:00 +0530

    Removed used methods

--- altimetrik-task-linkedin/src/test/java/com/altimetrik/utils/TestDataUtil.java
+++ altimetrik-task-linkedin/src/test/java/com/altimetrik/utils/TestDataUtil.java
@@ -23,19 +23,10 @@ public class TestDataUtil {
 		}
 	}
 
-	public String getStringData(int sheetIndex, int row, int column) {
-
-		return wb.getSheetAt(sheetIndex).getRow(row).getCell(column).getStringCellValue();
-
-	}
-
 	public String getStringData(String sheetName, int row, int column) {
 
 		return wb.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
 
 	}
 
-	public double getNumericData(String sheetName, int row, int column) {
-		return wb.getSheet(sheetName).getRow(row).getCell(column).getNumericCellValue();
-	}
 }
